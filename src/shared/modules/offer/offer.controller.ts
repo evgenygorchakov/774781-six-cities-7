c491842f5cdad0aa0629470856224b97829279b8
evgenygorchakov <stormdice23@gmail.com>
2024-07-20 00:34:48 +0500

    Add create method for offer controller

--- src/shared/modules/offer/offer.controller.ts
+++ src/shared/modules/offer/offer.controller.ts
@@ -1,11 +1,14 @@
 import { inject, injectable } from 'inversify';
 import { Request, Response } from 'express';
+
 import { BaseController, HttpMethod } from '../../libs/rest/index.js';
 import { Logger } from '../../libs/logger/index.js';
-import { Component } from '../../types/component.enum.js';
+import { Component } from '../../types/index.js';
+import { fillDTO } from '../../helpers/index.js';
+
 import { OfferService } from './offer-service.interface.js';
-import { fillDTO } from '../../helpers/common.js';
 import { OfferRdo } from './rdo/offer.rdo.js';
+import { CreateOfferDto } from './dto/create-offer.dto.js';
 
 @injectable()
 export class OfferController extends BaseController {
@@ -18,6 +21,7 @@ export class OfferController extends BaseController {
     this.logger.info('Register routes for OfferController…');
 
     this.addRoute({ path: '/', method: HttpMethod.Get, handler: this.index });
+    this.addRoute({ path: '/', method: HttpMethod.Post, handler: this.create });
   }
 
   public async index(_req: Request, res: Response) {
@@ -26,5 +30,11 @@ export class OfferController extends BaseController {
     this.ok(res, responseData);
   }
 
-  public async create(_req: Request, _res: Response) {}
+  public async create(
+    { body }: Request<Record<string, unknown>, Record<string, unknown>, CreateOfferDto>,
+    res: Response,
+  ): Promise<void> {
+    const result = await this.offerService.create(body);
+    this.created(res, fillDTO(OfferRdo, result));
+  }
 }
