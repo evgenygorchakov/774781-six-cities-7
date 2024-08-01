1b29dd5c6e2deb44ad6ff39bde829d2edb2c941d
evgenygorchakov <stormdice23@gmail.com>
2024-08-01 15:05:46 +0500

    Typo

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -71,7 +71,7 @@ export class RestApplication {
 
     this.logger.info('Init exception filters');
     await this.initExceptionFilters();
-    this.logger.info('Exception filters initialization compleated');
+    this.logger.info('Exception filters initialization completed');
 
     this.logger.info('Try to init server…');
     await this.initServer();
