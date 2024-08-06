6d9de79101a09c72b5a5c8d220b3b7382a7cac64
evgenygorchakov <stormdice23@gmail.com>
2024-08-06 21:38:52 +0500

    Fix formatting

--- src/shared/modules/user/user.container.ts
+++ src/shared/modules/user/user.container.ts
@@ -1,18 +1,19 @@
 import { Container } from 'inversify';
+import { types } from '@typegoose/typegoose';
 
 import { Component } from '../../types/index.js';
+import { Controller } from '../../libs/rest/index.js';
 import { DefaultUserService } from './default-user.service.js';
 import { UserService } from './user-service.interface.js';
 import { UserEntity, UserModel } from './user.entity.js';
-import { types } from '@typegoose/typegoose';
-import { Controller } from '../../libs/rest/index.js';
 import { UserController } from './user.controller.js';
 
 export function createUserContainer() {
   const userContainer = new Container();
 
   userContainer
-    .bind<UserService>(Component.UserService).to(DefaultUserService)
+    .bind<UserService>(Component.UserService)
+    .to(DefaultUserService)
     .inSingletonScope();
 
   userContainer
@@ -20,7 +21,8 @@ export function createUserContainer() {
     .toConstantValue(UserModel);
 
   userContainer
-    .bind<Controller>(Component.UserController).to(UserController)
+    .bind<Controller>(Component.UserController)
+    .to(UserController)
     .inSingletonScope();
 
   return userContainer;
