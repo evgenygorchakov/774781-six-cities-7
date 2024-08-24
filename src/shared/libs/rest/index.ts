15e78de70ef2c152533974fb4c81629e13778b74
evgenygorchakov <stormdice23@gmail.com>
2024-08-24 17:50:23 +0500

    Add middlewares in route

--- src/shared/libs/rest/index.ts
+++ src/shared/libs/rest/index.ts
@@ -7,3 +7,4 @@ export { BaseController } from './controller/base-controller.abstract.js';
 export { ExceptionFilter } from './exception-filter/exception-filter.interface.js';
 export { AppExceptionFilter } from './exception-filter/app-exception-filter.js';
 export { HttpError } from './errors/index.js';
+export { Middleware } from './middleware/middleware.interface.js';
