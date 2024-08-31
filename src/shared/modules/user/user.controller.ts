f710c297d8fcb07d9cb61706b546c644d7fd8c6b
evgenygorchakov <stormdice23@gmail.com>
2024-08-31 17:21:51 +0500

    Fix formatting

--- src/shared/modules/user/user.controller.ts
+++ src/shared/modules/user/user.controller.ts
@@ -2,7 +2,11 @@ import { injectable, inject } from 'inversify';
 import { Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
-import { BaseController, HttpError, HttpMethod } from '../../libs/rest/index.js';
+import {
+  BaseController,
+  HttpError,
+  HttpMethod,
+} from '../../libs/rest/index.js';
 import { Logger } from '../../libs/logger/index.js';
 import { Component } from '../../types/component.enum.js';
 import { UserService } from './user-service.interface.js';
@@ -17,18 +21,26 @@ export class UserController extends BaseController {
   constructor(
     @inject(Component.Logger) protected readonly logger: Logger,
     @inject(Component.UserService) private readonly userService: UserService,
-    @inject(Component.Config) private readonly configService: Config<RestSchema>,
+    @inject(Component.Config) private readonly configService: Config<RestSchema>
   ) {
     super(logger);
     this.logger.info('Register routes for UserController…');
 
-    this.addRoute({ path: '/register', method: HttpMethod.Post, handler: this.create });
-    this.addRoute({ path: '/login', method: HttpMethod.Post, handler: this.login });
+    this.addRoute({
+      path: '/register',
+      method: HttpMethod.Post,
+      handler: this.create,
+    });
+    this.addRoute({
+      path: '/login',
+      method: HttpMethod.Post,
+      handler: this.login,
+    });
   }
 
   public async create(
     { body }: CreateUserRequest,
-    res: Response,
+    res: Response
   ): Promise<void> {
     const existsUser = await this.userService.findByEmail(body.email);
 
@@ -40,28 +52,31 @@ export class UserController extends BaseController {
       );
     }
 
-    const result = await this.userService.create(body, this.configService.get('SALT'));
+    const result = await this.userService.create(
+      body,
+      this.configService.get('SALT')
+    );
     this.created(res, fillDTO(UserRdo, result));
   }
 
   public async login(
     { body }: LoginUserRequest,
-    _res: Response,
+    _res: Response
   ): Promise<void> {
     const existsUser = await this.userService.findByEmail(body.email);
 
-    if (! existsUser) {
+    if (!existsUser) {
       throw new HttpError(
         StatusCodes.UNAUTHORIZED,
         `User with email ${body.email} not found.`,
-        'UserController',
+        'UserController'
       );
     }
 
     throw new HttpError(
       StatusCodes.NOT_IMPLEMENTED,
       'Not implemented',
-      'UserController',
+      'UserController'
     );
   }
 }
