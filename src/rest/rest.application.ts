c05558387a3b7161882f41ab9c5ec79c13ecc00c
evgenygorchakov <stormdice23@gmail.com>
2024-12-01 16:18:39 +0500

    Add HOST var

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -6,7 +6,7 @@ import { RestSchema } from '../shared/libs/config/index.js';
 import { Logger } from '../shared/libs/logger/index.js';
 import { Component } from '../shared/types/index.js';
 import { DatabaseClient } from '../shared/libs/database-client/index.js';
-import { getMongoURI } from '../shared/helpers/index.js';
+import { getFullServerPath, getMongoURI } from '../shared/helpers/index.js';
 import { Controller, ExceptionFilter } from '../shared/libs/rest/index.js';
 import { ParseTokenMiddleware } from '../shared/libs/rest/middleware/parse-token.middleware.js';
 
@@ -88,6 +88,6 @@ export class RestApplication {
 
     this.logger.info('Try to init server…');
     await this.initServer();
-    this.logger.info(`🚀 Server started on http://localhost:${this.config.get('PORT')}`);
+    this.logger.info(`🚀 Server started on ${getFullServerPath(this.config.get('HOST'), this.config.get('PORT'))}`);
   }
 }
