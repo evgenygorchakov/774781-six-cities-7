bc03d672f293129995a642bce65bde028e036f35
evgenygorchakov <stormdice23@gmail.com>
2024-12-01 17:01:18 +0500

    Add static folder

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -51,6 +51,7 @@ export class RestApplication {
 
     this.server.use(express.json());
     this.server.use('/upload', express.static(this.config.get('UPLOAD_DIRECTORY')));
+    this.server.use('/static', express.static(this.config.get('STATIC_DIRECTORY_PATH')));
     this.server.use(authenticateMiddleware.execute.bind(authenticateMiddleware));
   }
 
